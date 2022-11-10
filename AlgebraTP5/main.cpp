2096ee4e4246f745d77fc46210bd135c0e0cf712
Enzo <coletta.enzo@yahoo.com.ar>
2022-11-10 09:51:49 -0300

    add all .cpp and .h

--- AlgebraTP5/main.cpp
+++ AlgebraTP5/main.cpp
@@ -2,8 +2,22 @@
 
 int main()
 {
-	
-	InitWindow(900, 1080, "pool");
+	InitWindow(900, 900, "pool");
 
+	while (true)
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 10, WHITE);
+
+		DrawRectangleLines(150, 50, 600, 800, WHITE);
+
+		DrawCircle(160, 60, 10, WHITE);
+		DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 10, WHITE);
+		DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 10, WHITE);
+		DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 10, WHITE);
+
+		EndDrawing();
+	}
 	return 0;
 }
